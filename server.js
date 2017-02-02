6d4f07fa6e3ecdea45edc2e2d0a3b2a16edb3cec
Ubuntu <ubuntu@ip-172-31-62-169.ec2.internal>
2017-02-02 02:31:17 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var express = require('express');
+var http = require('http');
+var https = require('https');
+var compression = require('compression');
+var path = require('path');
+
+var sslKey = fs.readFileSync('letsencrypt/privkey.pem', 'utf8');
+var sslCert = fs.readFileSync('letsencrypt/cert.pem', 'utf8');
+
+var creds = { key: sslKey, cert: sslCert };
+var app = express();
+app.use(compression());
+
+app.get('/background.jpg', function(request, response) {
+    response.sendFile(path.join(__dirname+'/public/background.jpg'));
+});
+
+app.get('/nj.png', function(request, response) {
+    response.sendFile(path.join(__dirname+'/public/nj.png'));
+});
+
+app.get('/sitemap.txt', function(request, response) {
+    response.sendFile(path.join(__dirname+'/public/sitemap.txt'));
+});
+
+app.get('*', function(request, response) {
+    response.sendFile(path.join(__dirname+'/public/home.html'));
+    console.log(request.ip + ' ' + (new Date()));
+});
+
+http.createServer(app).listen(8080);
+https.createServer(creds, app).listen(8443);
